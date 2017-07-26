444fbeae2074c55c5c5f9dad195a163b6e8481a4
Joseph Paul <mail@jsph.pl>
2017-07-26 18:57:53 +0200

    Eager-load comments; Add docblocks to controller methods

--- app/Api/V1/Controllers/ArticlesController.php
+++ app/Api/V1/Controllers/ArticlesController.php
@@ -6,5 +6,17 @@ use App\Models\Article;
 
 class ArticlesController extends BaseController
 {
+    /**
+     * The model that is exposed through this controller.
+     *
+     * @var string
+     */
     protected $model = Article::class;
+
+    /**
+     * The relations that should be eager-loaded and contained in the response.
+     *
+     * @var array
+     */
+    protected $relations = ['comments'];
 }
--- app/Api/V1/Controllers/CommentsController.php
+++ app/Api/V1/Controllers/CommentsController.php
@@ -6,5 +6,10 @@ use App\Models\Comment;
 
 class CommentsController extends BaseController
 {
+    /**
+     * The model that is exposed through this controller.
+     *
+     * @var string
+     */
     protected $model = Comment::class;
 }
