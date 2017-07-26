497a0a808d7205aaf0db6b3e5300cff06fa8f7aa
Joseph Paul <mail@jsph.pl>
2017-07-26 18:59:48 +0200

    Enable RelationSplitter in BaseController

--- app/Api/V1/Controllers/BaseController.php
+++ app/Api/V1/Controllers/BaseController.php
@@ -4,11 +4,16 @@ namespace App\Api\V1\Controllers;
 
 use App\Api\V1\ModelMapping;
 use SehrGut\Laravel5_Api\Controller;
+use SehrGut\Laravel5_Api\Plugins\RelationSplitter;
 
 class BaseController extends Controller
 {
-    /** {@inheritdoc} */
-    protected $plugins = [];
+    /**
+     * Plugins that should be loaded in this controller.
+     *
+     * @var array
+     */
+    protected $plugins = [RelationSplitter::class];
 
     /** {@inheritdoc} */
     protected $model_mapping_class = ModelMapping::class;
